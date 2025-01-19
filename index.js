971bcb65552f3474f6e72534d334f9b2396b8fd4
Moe-Karim <mohamadkarimfahs@gmail.com>
2025-01-19 11:33:51 +0200

    Edited: Fixed audio path name <> JS

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.post("/upload", upload.single("video"), async (req, res) => {
         console.log("📌 Processing started...");
 
         const audioPath = await extractAudio(videoPath, outputDir);
-        const { topics, content, transcriptFile } = await transcribeAudioWithGroq(audioPath);
+        const { topics, content, transcriptFile } = await transcribeAudioWithGroq(`${audioPath}.mp3`);
         const videoSegments = await segmentVideoBasedOnTimestamps(videoPath, topics, outputDir);
     
         res.json({ message: "✅ Video segmented successfully!", segments: videoSegments, topics, content, transcript: transcriptFile });
