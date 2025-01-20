70fc0f3b847077da2659a6518468abcdfeee9594
Moe-Karim <mohamadkarimfahs@gmail.com>
2025-01-20 19:00:03 +0200

    Edited: Output dir added recursion

--- index.js
+++ index.js
@@ -19,8 +19,7 @@ app.post("/upload", upload.single("video"), async (req, res) => {
     const videoPath = req.file.path;
     const outputDir = `processed/${currentDate}/`;
 
-    fs.mkdirSync(outputDir);
-
+    fs.mkdirSync('processed', { recursive: true });
     try {
         console.log("📌 Processing started...");
 
