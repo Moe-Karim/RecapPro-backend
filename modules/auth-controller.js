a381380fdebf036616850272c8170de9b143fa13
Moe-Karim <mohamadkarimfahs@gmail.com>
2025-01-21 22:56:09 +0200

    Created: Register user controller <> JS

--- modules/auth-controller.js
+++ modules/auth-controller.js
@@ -27,3 +27,21 @@ export const login = async (req, res) => {
     token,
   });
 };
+export const register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const hashed = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      name,
+      email,
+      password: hashed,
+    });
+    return res.status(201).send(user);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({
+      message: "Something went wrong",
+    });
+  }
+};
