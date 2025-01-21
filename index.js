de89c6d52e53719db8261c9419b01d41db838468
Moe-Karim <mohamadkarimfahs@gmail.com>
2025-01-21 09:00:33 +0200

    Added: Transcribe endpoint

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const PORT = 3000;
 
 app.use(express.json());
 
+app.use("/transcribe", transcribeRoute);
+
 
 const processedPath = path.resolve('processed');
 fs.mkdirSync(processedPath, { recursive: true });
