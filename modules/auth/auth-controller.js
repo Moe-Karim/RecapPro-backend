9f990f0f2ae3918c52cebc0bbcc1bddf45cf9692
Moe-Karim <mohamadkarimfahs@gmail.com>
2025-01-22 13:37:34 +0200

    Created: Change password logic

--- modules/auth/auth-controller.js
+++ modules/auth/auth-controller.js
@@ -1,5 +1,6 @@
 import { User } from "../../db/models/user-model.js";
 import bcrypt from "bcrypt";
+import { json } from "express";
 import jwt from "jsonwebtoken";
 
 export const login = async (req, res) => {
@@ -27,6 +28,8 @@ export const login = async (req, res) => {
     token,
   });
 };
+
+
 export const register = async (req, res) => {
   const { name, email, password } = req.body;
 
@@ -43,5 +46,38 @@ export const register = async (req, res) => {
     res.status(500).send({
       message: "Something went wrong",
     });
+  }
+};
+
+
+export const changePassword = async (req, res) => {
+  const token = req.headers.authorization.split(' ')[1];
+  const {currentPassword, newPassword} = req.body;
+
+  try {
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const userId = decoded.id;
+    const user = await User.findById(userId).select("+password");
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const isMatch = await bcrypt.compare(currentPassword, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Incorrect current password" });
+    }
+
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+
+    user.password = hashedPassword;
+    await user.save();
+
+    return res.status(200).json({ message: "Password updated successfully" });
+  } catch (error) {
+    console.error("Error changing password:", error);
+    return res.status(500).json({ message: "An error occurred while updating password" });
   }
 };
