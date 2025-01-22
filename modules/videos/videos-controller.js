79bbd0a4d98cc14fd33084f7924516b8de49d5f1
Moe-Karim <mohamadkarimfahs@gmail.com>
2025-01-22 16:06:52 +0200

    Updated: Videos controller added get video

--- modules/videos/videos-controller.js
+++ modules/videos/videos-controller.js
@@ -27,3 +27,30 @@ export const uploadVideo = async (req, res) => {
       .json({ message: "An error occurred while uploading the video" });
   }
 };
+
+export const getVideo = async (req, res) => {
+  const token = req.headers.authorization.split(" ")[1];
+  const { videoId } = req.params;
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const userId = decoded.id;
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const video = user.videos.id(videoId);
+    if (!video) {
+      return res.status(404).json({ message: "Video not found" });
+    }
+
+    return res.status(200).json({ video });
+  } catch (error) {
+    console.error("Error fetching video:", error);
+    return res
+      .status(500)
+      .json({ message: "An error occurred while fetching the video" });
+  }
+};
