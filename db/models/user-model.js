acaaa20b7c059204857dbb389edf80d1085e0c8c
Moe-Karim <mohamadkarimfahs@gmail.com>
2025-01-22 15:17:39 +0200

    Edited: User schema added videos

--- db/models/user-model.js
+++ db/models/user-model.js
@@ -1,21 +1,27 @@
 import { Schema, model } from "mongoose";
 
 const userSchema = new Schema({
-    name:{
-        type:String,
-        required:true,
+    name: {
+        type: String,
+        required: true,
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
+    email: {
+        type: String,
+        required: true,
+        unique: true,
     },
-    password:{
-        type:String,
-        required:true,
-        select:false,
-    }
+    password: {
+        type: String,
+        required: true,
+        select: false,
+    },
+    videos: [
+        {
+            videoId: { type: Schema.Types.ObjectId, auto: true },
+            path: { type: String, required: true },
+            createdAt: { type: Date, default: Date.now },
+        }
+    ]
 });
-
 
 export const User = model("User", userSchema);
